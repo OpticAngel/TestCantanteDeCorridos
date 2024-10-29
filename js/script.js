cf1934dfd59da8c232ce20c85fb4c34e845add1d
OpticAngel <parteroangel@gmail.com>
2024-10-29 01:25:11 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,31 +12,31 @@ new Vue({
                 text: "¿Qué tema prefieres en tus canciones?",
                 options: [
                     { text: "Historias de superación y esfuerzo", value: 'A' },
-                    { text: "Reflexiones sobre la vida en el rancho", value: 'B' },
+                    { text: "Reflexiones sobre la vida", value: 'B' },
                     { text: "La vida de éxito y lujos", value: 'C' }
                 ]
             },
             {
                 text: "¿Qué estilo de ropa te identifica más?",
                 options: [
-                    { text: "Estilo urbano y relajado", value: 'A' },
-                    { text: "Estilo vaquero con botas y sombrero", value: 'B' },
-                    { text: "Ropa de diseñador y accesorios llamativos", value: 'C' }
+                    { text: "Estilo urbano", value: 'A' },
+                    { text: "Estilo vaquero", value: 'B' },
+                    { text: "Estilo llamativo", value: 'C' }
                 ]
             },
             {
                 text: "¿Cuál es tu bebida favorita para acompañar una buena canción?",
                 options: [
-                    { text: "Cerveza bien fría", value: 'A' },
-                    { text: "Tequila o mezcal", value: 'B' },
+                    { text: "Cerveza", value: 'A' },
+                    { text: "Tequila", value: 'B' },
                     { text: "Whisky", value: 'C' }
                 ]
             },
             {
                 text: "¿Qué es lo más importante en tu vida?",
                 options: [
-                    { text: "El apoyo de mi familia y amigos", value: 'A' },
-                    { text: "Mis raíces y tradiciones", value: 'B' },
+                    { text: "Familia", value: 'A' },
+                    { text: "Mis tradiciones", value: 'B' },
                     { text: "Lograr éxito y reconocimiento", value: 'C' }
                 ]
             }
